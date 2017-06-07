a479bac9f8d615e8eb894d13684bb40e8658776c
Curriosityy <patryk9595@gmail.com>
2017-06-07 19:23:27 +0200

    little change in database

--- blog.sql
+++ blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 03 Maj 2017, 15:56
+-- Czas generowania: 07 Cze 2017, 19:22
 -- Wersja serwera: 10.1.21-MariaDB
 -- Wersja PHP: 5.6.30
 
@@ -28,10 +28,19 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `artykuly` (
   `id_artykulu` int(10) NOT NULL,
-  `nazwa` text COLLATE armscii8_bin NOT NULL,
-  `artykul` text COLLATE armscii8_bin NOT NULL,
-  `data` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
+  `nazwa` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `artykul` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `data` datetime NOT NULL,
+  `IloscWejsc` int(8) UNSIGNED NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ROW_FORMAT=COMPACT;
+
+--
+-- Zrzut danych tabeli `artykuly`
+--
+
+INSERT INTO `artykuly` (`id_artykulu`, `nazwa`, `artykul`, `data`, `IloscWejsc`) VALUES
+(14, 'asd1123', 'asd1123123', '2017-06-07 14:54:37', 65),
+(16, 'moidyfik', '123', '2017-06-07 16:30:58', 67);
 
 -- --------------------------------------------------------
 
@@ -43,10 +52,18 @@ CREATE TABLE `komentarze` (
   `id_komentarza` int(10) NOT NULL,
   `id_art` int(10) NOT NULL,
   `id_konta` int(10) NOT NULL,
-  `id_kom` int(10) DEFAULT NULL,
   `data` date NOT NULL,
-  `komentarz` text COLLATE armscii8_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
+  `komentarz` text COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci ROW_FORMAT=COMPACT;
+
+--
+-- Zrzut danych tabeli `komentarze`
+--
+
+INSERT INTO `komentarze` (`id_komentarza`, `id_art`, `id_konta`, `data`, `komentarz`) VALUES
+(14, 16, 9, '2017-06-07', '123'),
+(15, 16, 9, '2017-06-07', '1234'),
+(16, 14, 11, '2017-06-07', 'aaaaa123123');
 
 -- --------------------------------------------------------
 
@@ -56,11 +73,21 @@ CREATE TABLE `komentarze` (
 
 CREATE TABLE `konta` (
   `id_konta` int(10) NOT NULL,
-  `nick` varchar(25) COLLATE armscii8_bin NOT NULL,
-  `email` varchar(25) COLLATE armscii8_bin NOT NULL,
+  `nick` varchar(25) COLLATE utf8_polish_ci NOT NULL,
+  `email` varchar(25) COLLATE utf8_polish_ci NOT NULL,
   `admin` int(1) NOT NULL DEFAULT '0',
-  `haslo` varchar(25) COLLATE armscii8_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
+  `haslo` varchar(64) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci ROW_FORMAT=COMPACT;
+
+--
+-- Zrzut danych tabeli `konta`
+--
+
+INSERT INTO `konta` (`id_konta`, `nick`, `email`, `admin`, `haslo`) VALUES
+(1, 'admin', 'admin@admin.pl', 1, '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918'),
+(9, 'zenon', '1@1.pl', 0, '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b'),
+(10, 'zenon2', 'zenon@z.pl', 0, '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b'),
+(11, 'a', 'a@a.pl', 0, 'ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -78,8 +105,7 @@ ALTER TABLE `artykuly`
 ALTER TABLE `komentarze`
   ADD PRIMARY KEY (`id_komentarza`),
   ADD KEY `id_art` (`id_art`),
-  ADD KEY `id_konta` (`id_konta`),
-  ADD KEY `id_kom` (`id_kom`);
+  ADD KEY `id_konta` (`id_konta`);
 
 --
 -- Indexes for table `konta`
@@ -97,12 +123,17 @@ ALTER TABLE `konta`
 -- AUTO_INCREMENT dla tabeli `artykuly`
 --
 ALTER TABLE `artykuly`
-  MODIFY `id_artykulu` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_artykulu` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+--
+-- AUTO_INCREMENT dla tabeli `komentarze`
+--
+ALTER TABLE `komentarze`
+  MODIFY `id_komentarza` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 --
 -- AUTO_INCREMENT dla tabeli `konta`
 --
 ALTER TABLE `konta`
-  MODIFY `id_konta` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_konta` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- Ograniczenia dla zrzutów tabel
 --
@@ -111,10 +142,8 @@ ALTER TABLE `konta`
 -- Ograniczenia dla tabeli `komentarze`
 --
 ALTER TABLE `komentarze`
-  ADD CONSTRAINT `komentarze_ibfk_1` FOREIGN KEY (`id_kom`) REFERENCES `komentarze` (`id_komentarza`),
   ADD CONSTRAINT `komentarze_ibfk_2` FOREIGN KEY (`id_art`) REFERENCES `artykuly` (`id_artykulu`),
-  ADD CONSTRAINT `komentarze_ibfk_3` FOREIGN KEY (`id_konta`) REFERENCES `konta` (`id_konta`),
-  ADD CONSTRAINT `komentarze_ibfk_4` FOREIGN KEY (`id_komentarza`) REFERENCES `komentarze` (`id_kom`);
+  ADD CONSTRAINT `komentarze_ibfk_3` FOREIGN KEY (`id_konta`) REFERENCES `konta` (`id_konta`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
